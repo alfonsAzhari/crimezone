67d6ae976ca9fc0ff8764e4654266a150fb87931
henrasn <henra.sn@gmail.com>
2015-03-02 21:44:36 +0700

    besok lanjut lagi coy

---
+++ app/src/main/java/com/Codelabs/crimezone/PostKejahatan.java
@@ -0,0 +1,145 @@
+package com.Codelabs.crimezone;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+public class PostKejahatan extends ActionBarActivity {
+
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE=100;
+    public static final int MEDIA_TYPE_IMAGE=1;
+    private static final String IMAGE_DIRECTORY_NAME="Crimezone";
+
+    private Uri fileUri;
+    private ImageView imageView;
+    private Button save;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_kejahatan);
+
+        declareView();
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureImage();
+            }
+        });
+    }
+
+    private void captureImage() {
+        Intent intent =  new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        fileUri=getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,fileUri);
+
+        startActivityForResult(intent,CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+    }
+
+    private void declareView() {
+        imageView=(ImageView)findViewById(R.id.imgView_add);
+        save=(Button)findViewById(R.id.btn_addImage);
+    }
+
+    public Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode==100){
+            if(resultCode==RESULT_OK){
+                previewCaptureImage();
+            }else if (resultCode==RESULT_CANCELED){
+                Toast.makeText(this,"Canceled",Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void previewCaptureImage() {
+        try{
+            BitmapFactory.Options options=new BitmapFactory.Options();
+
+            options.inSampleSize=8;
+
+            final Bitmap bitmap=BitmapFactory.decodeFile(fileUri.getPath(),options);
+
+            imageView.setImageBitmap(bitmap);
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_post_kejahatan, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private static File getOutputMediaFile(int type) {
+
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),IMAGE_DIRECTORY_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
+                        + IMAGE_DIRECTORY_NAME + " directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "IMG_" + timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+}
